2ef7c5f2a0388f7459458e33d1b6bb48947de895
Christian Berendt <berendt@osism.tech>
2021-11-27 13:01:00 +0100

    Support celery
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,10 +2,16 @@
 
 printenv > /etc/environment
 
+CELERY=${CELERY:-0}
+
 # NOTE: Really ugly. For whatever reason, the ansible-playbook running as
 #       dragon wants to access /root. To store configuration files.
 #       Setting ANSIBLE_DIRECTORY etc. does nothing.
 chmod 777 /root
 sudo su - dragon sh -c /run.sh
 
-exec /usr/sbin/crond -f -d 8
+if [[ "${CELERY}" == 0 ]]; then
+    exec /usr/sbin/crond -f -d 8
+elif [[ "${CELERY}" == 1 ]]; then
+    exec osism reconciler
+fi
