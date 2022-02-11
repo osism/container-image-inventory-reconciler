ccaca1b3ec8d81337fb10c19f9a35b0b59d3dac5
Christian Berendt <berendt@osism.tech>
2022-02-11 20:56:13 +0100

    Debug output the contents of /opt/configuration/inventory
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/run.sh
+++ files/run.sh
@@ -4,6 +4,8 @@ source /etc/environment
 
 rm -rf /inventory.pre/*
 
+ls -1 /opt/configuration/inventory/
+
 rsync -a --exclude README.md --exclude LICENSE --exclude '.*' /defaults/ /inventory.pre/group_vars/
 rsync -a /inventory.generics/ /inventory.pre/
 rsync -a /extra/ //inventory.pre/
