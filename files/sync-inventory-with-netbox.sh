f4dbc0fe802b32ae2eb8bb9e2277948c54ffc4cf
Christian Berendt <berendt@osism.tech>
2023-01-15 19:51:10 +0100

    Always sync the inventory with the netbox
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/sync-inventory-with-netbox.sh
+++ files/sync-inventory-with-netbox.sh
@@ -3,6 +3,4 @@
 source /etc/environment
 export NETBOX_API
 
-if [[ -e /run/secrets/NETBOX_TOKEN ]]; then
-    flock -n /tmp/sync-inventory-with-netbox.lock -c 'ansible-playbook -i /inventory /ansible/playbooks/import-netbox.yml'
-fi
+flock -n /tmp/sync-inventory-with-netbox.lock -c 'ansible-playbook -i /inventory /ansible/playbooks/import-netbox.yml'
