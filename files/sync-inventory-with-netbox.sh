ae54ebe252497fb56425fe90fdd6468603bcda4b
Christian Berendt <berendt@osism.tech>
2023-02-01 17:28:44 +0100

    Revert "Use lockfile instead of flock (#85)" (#87)
    Does not work like expected.
    
    This reverts commit 48114f328075a9f0f48a9947969483060e6ebe3f.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/sync-inventory-with-netbox.sh
+++ files/sync-inventory-with-netbox.sh
@@ -3,23 +3,4 @@
 source /etc/environment
 export NETBOX_API
 
-# NOTE: locking inspired by https://www.baeldung.com/linux/bash-ensure-instance-running
-
-LOCK_FILE=/tmp/sync-inventory-with-netbox.lock
-LOCK_TIMEOUT=300
-
-remove_lock()
-{
-    rm -f "$LOCK_FILE"
-}
-
-another_instance()
-{
-    echo "There is another instance running, exiting"
-    exit 1
-}
-
-lockfile -r 0 -l $LOCK_TIMEOUT "$LOCK_FILE" || another_instance
-trap remove_lock EXIT
-
-ansible-playbook -i /inventory /ansible/playbooks/import-netbox.yml
+flock -n /tmp/sync-inventory-with-netbox.lock -c 'ansible-playbook -i /inventory /ansible/playbooks/import-netbox.yml'
