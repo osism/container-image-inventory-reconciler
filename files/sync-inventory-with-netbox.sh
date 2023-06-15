bfb98077211445aac8d7aa66fa09a8878faf3cde
Christian Berendt <berendt@osism.tech>
2023-06-15 15:11:25 +0200

    Fix some issues when netbox is not enabled
    Part of osism/issues#553
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/sync-inventory-with-netbox.sh
+++ files/sync-inventory-with-netbox.sh
@@ -6,4 +6,4 @@ fi
 
 export NETBOX_API
 
-flock -n /tmp/sync-inventory-with-netbox.lock -c 'ansible-playbook -i /inventory /ansible/playbooks/import-netbox.yml'
+flock -n /tmp/sync-inventory-with-netbox.lock -c 'ansible-playbook -i /inventory/hosts.yml /ansible/playbooks/import-netbox.yml'
