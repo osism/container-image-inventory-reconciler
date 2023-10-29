b03b8d01c637ff958b158dd87bab4a22df4f7900
Christian Berendt <berendt@osism.tech>
2023-10-29 12:33:57 +0100

    Remove some output from the run.sh script (#158)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/run.sh
+++ files/run.sh
@@ -17,12 +17,10 @@ fi
 
 rm -rf /inventory.pre/*
 
-ls -1 /opt/configuration/inventory/
-
-rsync -a --exclude README.md --exclude LICENSE --exclude '.*' /defaults/ /inventory.pre/group_vars/
-rsync -a /inventory.generics/ /inventory.pre/
-rsync -a /extra/ /inventory.pre/
-rsync -a /opt/configuration/inventory/ /inventory.pre/
+rsync -q -a --exclude README.md --exclude LICENSE --exclude '.*' /defaults/ /inventory.pre/group_vars/
+rsync -q -a /inventory.generics/ /inventory.pre/
+rsync -q -a /extra/ /inventory.pre/
+rsync -q -a /opt/configuration/inventory/ /inventory.pre/
 
 # get version files from /interface/versions
 if [[ -e /interface/versions/osism-ansible.yml ]]; then
@@ -56,7 +54,7 @@ fi
 # The intermediate step via the inventory.merge directory
 # is necessary to remove other files in /inventory via -delete.
 ansible-inventory -i /inventory.pre --list -y --output /inventory.merge/hosts.yml
-rsync -a --delete --exclude .git /inventory.merge/ /inventory
+rsync -q -a --delete --exclude .git /inventory.merge/ /inventory
 
 pushd /inventory > /dev/null || exit 1
 
