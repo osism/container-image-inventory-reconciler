d7999f193b1d99ab4bda501463d02680fe06c411
Christian Berendt <berendt@osism.tech>
2023-10-30 21:07:42 +0100

    Use /python-osism in the change-osism.sh script
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/change-osism.sh
+++ files/change-osism.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-rm -rf /src
-git clone --depth 1 -b $1 https://github.com/osism/python-osism /src
+rm -rf /python-osism
+git clone --depth 1 -b $1 https://github.com/osism/python-osism /python-osism
 
-pushd /src
+pushd /python-osism
 pip3 uninstall -y osism
-python3 -m pip --no-cache-dir install -U /src
+python3 -m pip --no-cache-dir install -U /python-osism
 popd
