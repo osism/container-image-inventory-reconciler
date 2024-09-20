a5fb931c4c90030bac4d39ef66c635271d241c90
Christian Berendt <berendt@osism.tech>
2024-09-20 22:24:46 +0200

    Allow to overwrite inventory groups also with 20-roles & 99-netbox (#231)
    Related to osism/issues#1142
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/handle-inventory-overwrite.py
+++ files/handle-inventory-overwrite.py
@@ -1,60 +1,79 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# NOTE: This is a quick workaround.
+# This script reads a file in the /inventory.pre directory (if it exists).
 #
-# This script reads the file 99-overwrite in the /inventory.pre directory (if it exists).
+# Then it reads all other files (with exception of 99-overwrite) in /inventory.pre
+# and removes from them all sections that are present in the file. It considers
+# the variant with :children and without.
 #
-# Then it reads all other files in /inventory.pre and removes from them all sections that
-# are present in 99-overwrite. It considers the variant with :children and without.
-#
-# With this approach it is now possible to completely overwrite all group definitions from
-# existing files via the 99-overwrite file.
+# With this approach it is possible to overwrite all group definitions from
+# existing files with exception of 99-overwrite.
 
 import configparser
 import os
 import sys
 
-dirname = "/inventory.pre/"
-filename = "99-overwrite"
+from loguru import logger
 
-if not os.path.isfile(os.path.join(dirname, filename)):
-    sys.exit(0)
+level = "INFO"
+log_fmt = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | "
+    "<level>{message}</level>"
+)
 
-config = configparser.ConfigParser(allow_no_value=True, delimiters="=")
-config.read(os.path.join(dirname, filename))
+logger.remove()
+logger.add(sys.stdout, format=log_fmt, level=level, colorize=True)
 
-sections = []
 
-for section in config.sections():
-    if section.endswith(":children"):
-        sections.append(section[:-9])
-    else:
-        sections.append("%s:children" % section)
+def handle_overwrite_file(filename, dirname="/inventory.pre/"):
+    if not os.path.isfile(os.path.join(dirname, filename)):
+        return
 
-    sections.append(section)
+    logger.info(f"Handling group overwrites in {filename}")
 
-for f in os.scandir(dirname):
-    if (
-        f.is_file()
-        and not f.path.endswith(filename)
-        and not f.name.startswith(".")
-        and not f.name.endswith(".yml")
-        and not f.name.endswith(".yaml")
-    ):
-        changed = False
+    config = configparser.ConfigParser(allow_no_value=True, delimiters="=")
+    config.read(os.path.join(dirname, filename))
 
-        config = configparser.ConfigParser(allow_no_value=True, delimiters="=")
+    sections = []
 
-        try:
-            config.read(os.path.join(f))
-        except UnicodeDecodeError as e:
-            print(f"Syntax issue in file {f.name}: {e}")
-            sys.exit(1)
+    for section in config.sections():
+        if section.endswith(":children"):
+            sections.append(section[:-9])
+        else:
+            sections.append("%s:children" % section)
 
-        for section in sections:
-            if config.remove_section(section):
-                changed = True
+        sections.append(section)
 
-        if changed:
-            with open(f, "w") as fp:
-                config.write(fp)
+    for f in os.scandir(dirname):
+        if (
+            f.is_file()
+            and not f.path.endswith(filename)
+            and not f.path.endswith("99-overwrite")
+            and not f.name.startswith(".")
+            and not f.name.endswith(".yml")
+            and not f.name.endswith(".yaml")
+        ):
+            changed = False
+
+            config = configparser.ConfigParser(allow_no_value=True, delimiters="=")
+
+            try:
+                config.read(os.path.join(f))
+            except UnicodeDecodeError as e:
+                logger.error(f"Syntax issue in file {f.name}: {e}")
+                return
+
+            for section in sections:
+                if config.remove_section(section):
+                    logger.info(f"Removing group {section} from {f.name}")
+                    changed = True
+
+            if changed:
+                with open(f, "w") as fp:
+                    config.write(fp)
+
+
+if __name__ == "__main__":
+    handle_overwrite_file("99-overwrite")
+    handle_overwrite_file("99-netbox")
+    handle_overwrite_file("20-roles")
